6b04947b921ac231b5dce98fb5ca32a1d1b4596d
Usser65-87-11 <googa.vo1d@gmail.com>
2026-05-15 10:56:15 +0300

    up

--- debug_7seg.glsl
+++ debug_7seg.glsl
@@ -1,196 +1,92 @@
-/*
-HOW TO USE
+#define DRAW_DEC 10u
+#define DRAW_HEX 16u
 
-	vec2 debug_position = vec2(0.0,0.0);
-	
-	vec4 out_color = vec4(1.0,1.0,1.0,1.0);
+const uint DIGIT_MASK[16] = uint[16](
+    // 0-9
+    0x3F, // 0
+    0x06, // 1
+    0x5B, // 2
+    0x4F, // 3
+    0x66, // 4
+    0x6D, // 5
+    0x7D, // 6
+    0x07, // 7
+    0x7F, // 8
+    0x6F, // 9
 
-	float scale_factor = 0.5;
-
-	if (draw_9999(99, gl_FragCoord.xy, debug_position,scale_factor)) {
-		out_color  = vec4(1.0, 0.0, 0.0, 1.0);
-	}
-	
-
-	FragColor = out_color;
-*/
-#ifdef _OLD2_
-bool seg_a(vec2 v, vec2 o) {
-    return (v.x > o.x + 5 && v.x < o.x + 5 + 40 && v.y > o.y && v.y < o.y + 10);
-}
-bool seg_b(vec2 v, vec2 o) {
-    return (v.x > o.x + 40.0 && v.x < o.x + 40.0 + 10.0 && v.y > o.y + 10 && v.y < o.y + 10 + 40);
-}
-bool seg_c(vec2 v, vec2 o) {
-    return (v.x > o.x + 40.0 && v.x < o.x + 40.0 + 10.0 && v.y > o.y + 10 + 40 + 10 && v.y < o.y + 10 + 40 + 10 + 40);
-}
-bool seg_d(vec2 v, vec2 o) {
-    return (v.x > o.x + 5 && v.x < o.x + 5 + 40 && v.y > o.y + 10 + 40 + 10 + 40 && v.y < o.y + 10 + 40 + 10 + 40 + 10);
-}
-bool seg_e(vec2 v, vec2 o) {
-    return (v.x > o.x && v.x < o.x + 10 && v.y > o.y + 10 + 40 + 10 && v.y < o.y + 10 + 40 + 10 + 40);
-}
-bool seg_f(vec2 v, vec2 o) {
-    return (v.x > o.x && v.x < o.x + 10 && v.y > o.y + 10 && v.y < o.y + 10 + 40);
-}
-bool seg_g(vec2 v, vec2 o) {
-    return (v.x > o.x + 5 && v.x < o.x + 5 + 40 && v.y > o.y + 10 + 40 && v.y < o.y + 10 + 40 + 10);
-}
-bool draw_digit(uint digit, vec2 v, vec2 o)
+    // A-F
+    0x77, // A
+    0x7C, // b
+    0x39, // C
+    0x5E, // d
+    0x79, // E
+    0x71  // F
+);
+bool draw_digit(uint mask, vec2 v, vec2 o, float s)
 {
-    bool a = false, b = false, c = false, d = false, e = false, f = false, g = false;
-
-    if (digit == 0) {
-        a = f = b = c = d = e = true;
-    }
-    if (digit == 1) {
-        b = c = true;
-    }
-    if (digit == 2) {
-        a = b = g = e = d = true;
-    }
-    if (digit == 3) {
-        a = b = c = d = g = true;
-    }
-    if (digit == 4) {
-        f = g = b = c = true;
-    }
-    if (digit == 5) {
-        a = f = g = c = d = true;
-    }
-    if (digit == 6) {
-        a = f = g = e = c = d = true;
-    }
-    if (digit == 7) {
-        a = b = c = true;
-    }
-    if (digit == 8) {
-        a = b = c = d = e = f = g = true;
-    }
-    if (digit == 9) {
-        a = b = c = d = f = g = true;
-    }
-
-    return (a && seg_a(v, o)) ||
-        (b && seg_b(v, o)) ||
-        (c && seg_c(v, o)) ||
-        (d && seg_d(v, o)) ||
-        (e && seg_e(v, o)) ||
-        (f && seg_f(v, o)) ||
-        (g && seg_g(v, o));
-}
-
-bool draw_digit2(uint digit, vec2 v, vec2 o)
-{
-    uint mask = DIGIT_MASK[digit];
-
-    float x = v.x;
-    float y = v.y;
-
-    float ox = o.x;
-    float oy = o.y;
-
-    return
-    // a
-    ((mask & 1u) != 0u &&
-        x > ox + 5.0 && x < ox + 45.0 &&
-        y > oy + 0.0 && y < oy + 10.0)
-        || // b
-        ((mask & 2u) != 0u &&
-            x > ox + 40.0 && x < ox + 50.0 &&
-            y > oy + 10.0 && y < oy + 50.0)
-        || // c
-        ((mask & 4u) != 0u &&
-            x > ox + 40.0 && x < ox + 50.0 &&
-            y > oy + 60.0 && y < oy + 100.0)
-        || // d
-        ((mask & 8u) != 0u &&
-            x > ox + 5.0 && x < ox + 45.0 &&
-            y > oy + 100.0 && y < oy + 110.0)
-        || // e
-        ((mask & 16u) != 0u &&
-            x > ox + 0.0 && x < ox + 10.0 &&
-            y > oy + 60.0 && y < oy + 100.0)
-        || // f
-        ((mask & 32u) != 0u &&
-            x > ox + 0.0 && x < ox + 10.0 &&
-            y > oy + 10.0 && y < oy + 50.0)
-        || // g
-        ((mask & 64u) != 0u &&
-            x > ox + 5.0 && x < ox + 45.0 &&
-            y > oy + 50.0 && y < oy + 60.0);
-}
-#endif
-
-const uint DIGIT_MASK[10] = uint[10](
-        0x3F, 0x06, 0x5B, 0x4F, 0x66,
-        0x6D, 0x7D, 0x07, 0x7F, 0x6F
-    );
-
-bool draw_digit3(uint digit, vec2 v, vec2 o, float s)
-{
-    uint mask = DIGIT_MASK[digit];
-
-    // transform into local digit space
     vec2 p = (v - o) / s;
 
     float x = p.x;
     float y = p.y;
 
-    return ((mask & 1u) != 0u &&
-        x > 5.0 && x < 45.0 &&
-        y > 0.0 && y < 10.0)
+    return
+        ((mask & 1u) != 0u &&
+            x > 5.0 && x < 45.0 &&
+            y > 0.0 && y < 10.0)
+
         || ((mask & 2u) != 0u &&
             x > 40.0 && x < 50.0 &&
             y > 10.0 && y < 50.0)
+
         || ((mask & 4u) != 0u &&
             x > 40.0 && x < 50.0 &&
             y > 60.0 && y < 100.0)
+
         || ((mask & 8u) != 0u &&
             x > 5.0 && x < 45.0 &&
             y > 100.0 && y < 110.0)
+
         || ((mask & 16u) != 0u &&
             x > 0.0 && x < 10.0 &&
             y > 60.0 && y < 100.0)
+
         || ((mask & 32u) != 0u &&
             x > 0.0 && x < 10.0 &&
             y > 10.0 && y < 50.0)
+
         || ((mask & 64u) != 0u &&
             x > 5.0 && x < 45.0 &&
             y > 50.0 && y < 60.0);
 }
-
-bool draw_9999(uint val, vec2 v, vec2 o, float s)
+bool draw_value(uint val, vec2 current_frag, vec2 output_pos, float size, uint type)
 {
-    val = min(val, 9999u);
+    uint d0;
+    uint d1;
+    uint d2;
+    uint d3;
 
-    uint a = (val / 1000u) % 10u;
-    uint b = (val / 100u) % 10u;
-    uint c = (val / 10u) % 10u;
-    uint d = val % 10u;
+    if(type == DRAW_DEC)
+    {
+        val = min(val, 9999u);
 
-    float w = 60.0 * s;
+        d0 = DIGIT_MASK[(val / 1000u) % 10u];
+        d1 = DIGIT_MASK[(val / 100u) % 10u];
+        d2 = DIGIT_MASK[(val / 10u) % 10u];
+        d3 = DIGIT_MASK[val % 10u];
+    }
+    else // DRAW_HEX
+    {
+        d0 = DIGIT_MASK[(val >> 12u) & 0xFu];
+        d1 = DIGIT_MASK[(val >>  8u) & 0xFu];
+        d2 = DIGIT_MASK[(val >>  4u) & 0xFu];
+        d3 = DIGIT_MASK[(val >>  0u) & 0xFu];
+    }
 
-    return draw_digit3(a, v, o + vec2(0.0 * w, 0.0), s) ||
-        draw_digit3(b, v, o + vec2(1.0 * w, 0.0), s) ||
-        draw_digit3(c, v, o + vec2(2.0 * w, 0.0), s) ||
-        draw_digit3(d, v, o + vec2(3.0 * w, 0.0), s);
+    float w = 60.0 * size;
+
+    return
+        draw_digit(d0, current_frag, output_pos + vec2(0.0 * w, 0.0), size) ||
+        draw_digit(d1, current_frag, output_pos + vec2(1.0 * w, 0.0), size) ||
+        draw_digit(d2, current_frag, output_pos + vec2(2.0 * w, 0.0), size) ||
+        draw_digit(d3, current_frag, output_pos + vec2(3.0 * w, 0.0), size);
 }
-#ifdef _OLD2_
-bool draw_9999_2(uint val, vec2 v, vec2 o) {
-    val = min(val, 9999u);
-    uint a = (val / 1000u) % 10u;
-    uint b = (val / 100u) % 10u;
-    uint c = (val / 10u) % 10u;
-    uint d = val % 10u;
-
-    float y = o.y;
-
-    bool result =
-        draw_digit2(a, v, vec2(o.x + 0.0, y)) ||
-            draw_digit2(b, v, vec2(o.x + 60.0, y)) ||
-            draw_digit2(c, v, vec2(o.x + 120.0, y)) ||
-            draw_digit2(d, v, vec2(o.x + 180.0, y));
-    return result;
-}
-#endif
